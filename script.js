ccb102bceec383384714cb055bb81ae37300d499
Bryan Lamanna <lamannabryan@gmail.com>
2024-01-10 18:21:00 -0300

    update style and index

--- script.js
+++ script.js
@@ -6,9 +6,42 @@ const btns = document.querySelectorAll('.btn');
 const inputValue = document.querySelector('.input-value');
 const total = document.querySelector('.total');
 const combobox = document.querySelector('.select');
+const selected = document.querySelector('.selected');
 const menu = document.querySelector('.menu');
+const optionList = document.querySelectorAll('.li');
+const visa = 'assets/visa.png';
+const mastercard = 'assets/master.png';
+const card = 'assets/card.png';
+const cardNum = document.querySelector('.input-card');
 let menuvisible = false;
 
+cardNum.addEventListener('input', () => {
+    let card = cardNum.value;
+    var cardString = card.toString();
+    var fourOnes = cardString.substring(0, 4);
+  
+    if (fourOnes == '1234') {
+        document.querySelector('.cardIMG').src = 'assets/master.png';    
+        document.querySelector('.invalid-card').style.opacity = '0'; 
+    } else if (fourOnes == '4321') {
+        document.querySelector('.cardIMG').src = visa;
+        document.querySelector('.invalid-card').style.opacity = '0';
+    } else {
+        document.querySelector('.invalid-card').style.opacity = '1';
+    }
+})
+
+
+
+
+optionList.forEach((li) => {
+    li.addEventListener('click', () => {
+        combobox.style.width = '4em';
+        menu.style.display = 'none';
+        selected.textContent = li.textContent;
+        menuvisible = !menuvisible;
+    })
+})
 
 combobox.addEventListener('click', () => {
 
