68dff6418222c7c7c802687c0afa00ea0036aa7d
Bryan Lamanna <lamannabryan@gmail.com>
2024-01-10 14:34:43 -0300

    update index.html and create js basic functions

--- script.js
+++ script.js
@@ -4,18 +4,35 @@ const cardForm = document.querySelector('.credit-box');
 const pixForm = document.querySelector('.pix-box');
 const btns = document.querySelectorAll('.btn');
 const inputValue = document.querySelector('.input-value');
+const total = document.querySelector('.total');
+const combobox = document.querySelector('.select');
+const menu = document.querySelector('.menu');
+let menuvisible = false;
 
+
+combobox.addEventListener('click', () => {
+
+    if (menuvisible) {
+        menu.style.display = 'none';
+        
+    } else {
+        menu.style.display = 'block';
+    }
+
+    menuvisible = !menuvisible;
+
+})
 
 btns[0].addEventListener('click', () => {
 
     if (inputValue.value === '') {
         alert('Por favor, insira um valor!');
     }  else { 
-
     if (rdPix.checked) {
         cardForm.style.display = 'none';
         pixForm.style.display = 'block';
         btns[0].innerHTML = 'Mudar forma de pagamento'
+        calcPix();
     } else if (rdCard.checked) {
         pixForm.style.display = 'none';
         cardForm.style.display = 'block';
@@ -27,11 +44,15 @@ btns[0].addEventListener('click', () => {
 
 rdPix.addEventListener('click', () => {
     rdCard.checked = false;
-
 })
 
 rdCard.addEventListener('click', () => {
     rdPix.checked = false;
-
 })
+
+function calcPix() {
+    let value = document.querySelector('.input-value').value;
+    let result = value * 0.9;
+    total.innerHTML = `Total: R$ ${result}`;
+} 
 
