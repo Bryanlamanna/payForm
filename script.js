cfdcdbe64b38ed826099a5d948a889e58cdb827f
Bryan Lamanna <lamannabryan@gmail.com>
2024-01-10 12:35:39 -0300

    update index.html

--- script.js
+++ script.js
@@ -3,15 +3,26 @@ const rdCard = document.querySelector('.radio-credit');
 const cardForm = document.querySelector('.credit-box');
 const pixForm = document.querySelector('.pix-box');
 const btns = document.querySelectorAll('.btn');
+const inputValue = document.querySelector('.input-value');
+
 
 btns[0].addEventListener('click', () => {
+
+    if (inputValue.value === '') {
+        alert('Por favor, insira um valor!');
+    }  else { 
+
     if (rdPix.checked) {
         cardForm.style.display = 'none';
         pixForm.style.display = 'block';
+        btns[0].innerHTML = 'Mudar forma de pagamento'
     } else if (rdCard.checked) {
         pixForm.style.display = 'none';
         cardForm.style.display = 'block';
+        btns[0].innerHTML = 'Mudar forma de pagamento'
     }
+
+}
 })
 
 rdPix.addEventListener('click', () => {
